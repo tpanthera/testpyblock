d14bef2bb25040d6bff788d5f5f0e7e67ada3b04
tpanthera <bisht11390@gmail.com>
2018-08-31 11:37:45 +0530

    Create usedadvan.py

---
+++ usedadvan.py
@@ -0,0 +1,10 @@
+# Python advance concepts used
+# while solving questions in hacker earth this was common to use :
+# below code will take input from user , strip will remove white spaces , split() will return list after spliting 
+# the given separator by spaces , here its , map will convert the every elements in list to integer and return map object.
+# list constructor will convert the map object to list object.
+
+test = list(map(int,input().strip().split()))
+
+
+# Publisher/subscriber design pattern used during connected with IOT devices with mqtt protocol.
